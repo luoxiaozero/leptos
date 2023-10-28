afab1eb61495ee8c9d538ec742f143d2ef16459d
luoxiao <luoxiaozero@163.com>
2023-10-28 23:16:40 +0800

    feat: add AsyncCallback

--- leptos_reactive/src/callback.rs
+++ leptos_reactive/src/callback.rs
@@ -41,7 +41,7 @@
 //! Use `SyncCallback` when you want the function to be `Sync` and `Send`.
 
 use crate::{store_value, StoredValue};
-use std::{fmt, sync::Arc};
+use std::{fmt, future::Future, pin::Pin, rc::Rc, sync::Arc};
 
 /// A wrapper trait for calling callbacks.
 pub trait Callable<In: 'static, Out: 'static = ()> {
@@ -225,10 +225,99 @@ impl<In, Out> Fn<(In,)> for SyncCallback<In, Out> {
     }
 }
 
+/// Callbacks define a standard way to store functions and closures.
+///
+/// `Callback` asynchronous version
+///
+/// # Example
+/// ```
+/// # use leptos::*;
+/// # use leptos::AsyncCallback;
+/// #[component]
+/// fn MyComponent(
+///     #[prop(into)] on_add: AsyncCallback<i32, bool>,
+/// ) -> impl IntoView {
+///     let number = create_rw_signal(0);
+///     let on_click = move |_| {
+///         let allow = on_add.call(number.get_untracked()).await;
+///         if allow {
+///             number.update(|num| *num += 1);
+///         }
+///     };
+///     view! {
+///         <button on:click=on_click>
+///             "Click: "
+///             {move || number.get()}
+///         </button>
+///     }
+/// }
+///
+/// fn test() -> impl IntoView {
+///     let on_add = move |num| {
+///         async move {
+///             // Add asynchronous logic
+///             num != 3
+///         }
+///     };
+///     view! {
+///         <MyComponent on_add />
+///     }
+/// }
+/// ```
+pub struct AsyncCallback<In: 'static, Out: 'static = ()>(
+    #[allow(clippy::complexity)]
+    StoredValue<Rc<dyn Fn(In) -> Pin<Box<dyn Future<Output = Out>>>>>,
+);
+
+impl<In> fmt::Debug for AsyncCallback<In> {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        fmt.write_str("AsyncCallback")
+    }
+}
+
+impl<In, Out> Clone for AsyncCallback<In, Out> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<In, Out> Copy for AsyncCallback<In, Out> {}
+
+impl<In, Out> AsyncCallback<In, Out> {
+    /// Creates a new callback from the given function.
+    pub fn new<F, Fu>(f: F) -> Self
+    where
+        F: Fn(In) -> Fu + 'static,
+        Fu: Future<Output = Out> + 'static,
+    {
+        let f = Rc::new(move |input: In| {
+            let fut = f(input);
+            Box::pin(fut) as Pin<Box<dyn Future<Output = Out>>>
+        });
+        Self(StoredValue::new(f))
+    }
+
+    /// calls the callback with the specified argument.
+    pub async fn call(&self, input: In) -> Out {
+        let f = self.0.get_value();
+        f(input).await
+    }
+}
+
+impl<F, In, Fu, Out> From<F> for AsyncCallback<In, Out>
+where
+    F: Fn(In) -> Fu + 'static,
+    Fu: Future<Output = Out> + 'static,
+{
+    fn from(f: F) -> Self {
+        Self::new(f)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{
-        callback::{Callback, SyncCallback},
+        callback::{AsyncCallback, Callback, SyncCallback},
         create_runtime,
     };
 
@@ -246,6 +335,15 @@ mod tests {
     fn clone_sync_callback() {
         let rt = create_runtime();
         let callback = SyncCallback::new(move |_no_clone: NoClone| NoClone {});
+        let _cloned = callback.clone();
+        rt.dispose();
+    }
+
+    #[test]
+    fn clone_async_callback() {
+        let rt = create_runtime();
+        let callback =
+            AsyncCallback::new(move |_no_clone: NoClone| async { NoClone {} });
         let _cloned = callback.clone();
         rt.dispose();
     }
@@ -299,6 +397,32 @@ mod tests {
             })
             .into();
 
+        rt.dispose();
+    }
+
+    #[test]
+    fn async_callback_from() {
+        let rt = create_runtime();
+        let _callback: AsyncCallback<(), String> =
+            (|()| async { "test".to_string() }).into();
+        rt.dispose();
+    }
+
+    #[test]
+    fn async_callback_from_html() {
+        let rt = create_runtime();
+        use leptos::{
+            html::{HtmlElement, H1},
+            *,
+        };
+
+        let _callback: AsyncCallback<String, HtmlElement<H1>> =
+            (|x: String| async move {
+                view! {
+                    <h1>{x}</h1>
+                }
+            })
+            .into();
         rt.dispose();
     }
 }
